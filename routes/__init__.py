153334fcaff72656d2dc9a7e387593c6b6993084
Marcus Yanello <marcus@yanello.net>
2025-04-29 21:32:42 -0500

    expense routes

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,21 +1,18 @@
-from flask import Flask
+from flask import Flask  # noqa: D104
 
 from routes.auth_routes import auth_bp
 from routes.dashboard_routes import dashboard_bp
 from routes.demo_routes import demo_bp
+from routes.expense_routes import expense_bp
 from routes.timezone_routes import timezone_bp
 from routes.transaction_routes import transaction_bp
 
 
 def register_blueprints(app: Flask):
     """Register all route blueprints with the app."""
-    # from currency_routes import currency_bp
-    # from expense_routes import expense_bp
-    # from group_routes import group_bp
-    # from settlement_routes import settlement_bp
     app.register_blueprint(auth_bp, url_prefix="/auth")
     app.register_blueprint(dashboard_bp)
-    # app.register_blueprint(expense_bp, url_prefix="/expenses")
+    app.register_blueprint(expense_bp, url_prefix="/expenses")
     # app.register_blueprint(group_bp, url_prefix="/groups")
     # app.register_blueprint(admin_bp, url_prefix="/admin")
     # app.register_blueprint(settlement_bp, url_prefix="/settlements")
